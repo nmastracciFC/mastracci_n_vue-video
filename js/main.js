57f5759bd13e4799287cfe3a4c0a2d26c4c9b636
nmastracci <mastracci.natalie@gmail.com>
2018-03-16 11:20:32 -0400

    turning images into anchor tags and then stripping the videopath out

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 const vm = new Vue({
 	el:"#app",
 	data: {
+		mainmessage: "Welcome to my Vue Video App",
+		videodata: [
+		{
+			name: "Star Wars: The Force Awakens",
+			thumb: "forceawakens.jpg",
+			vidsource: "forceawakens.mp4",
+			description: "yet another Star Wars movie"
+		},
+		{
+			name: "Stranger Things",
+			thumb: "strangerthings.jpg",
+			vidsource: "strangerthings.mp4",
+			description: "Don't get lost in the upside-down"
+		},
+		{
+			name: "Marvel's The Avengers",
+			thumb: "avengers.jpg",
+			vidsource: "avengers.mp4",
+			description: "Black widow forever!"
+		}
+		]
 		
 	},
 	methods: {
-		
+		loadMovie(e) {
+			e.preventDefault();
+			// working inside an object so do not use equals
+			var dataKey = e.currentTarget.href.substring(e.currentTarget.href.lastIndexOf('/')+1);
+			currentData: this.videodata.filter(video => video.vidsource === dataKey);
+		}
 	}
 });
