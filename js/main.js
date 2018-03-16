4c54d42ef62aa2413bb480f0b47f4efe18229871
nmastracci <mastracci.natalie@gmail.com>
2018-03-16 10:38:51 -0400

    cleaning out other app

--- js/main.js
+++ js/main.js
@@ -1,29 +1,9 @@
 const vm = new Vue({
 	el:"#app",
 	data: {
-		welcomemessage: "Welcome to the Vue App!",
-		deliciousFruit: [
-		{
-			name: 'apple',
-			flavour: 'tasty'
-
-		},
-		{
-			name: 'oranges',
-			flavour: 'acidic'
-
-		},
-		{
-			name: 'pineapples',
-			flavour: 'sour'
-
-		}],
-		hasVue: true,
-		vuemessage: "Vue's here!"
+		
 	},
 	methods: {
-		logClicked(e) {
-			console.log(e.currentTarget, this);
-		}
+		
 	}
 });
